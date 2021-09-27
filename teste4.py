0de8976e42955e051c47c43472fc9df06c31921a
Felipe Menezes Sacramento <84421730+Felipe-exec@users.noreply.github.com>
2021-09-27 20:45:57 -0300

    Delete teste4.py

--- teste4.py
+++
@@ -1,8 +0,0 @@
-tempo = int(input('Quantos anos tem seu carro?'))
-print('carro novo'if tempo<=3 else'carro cagado')
-nome = str(input('Qual é o seu nome?')).strip()
-if nome == 'Felipe':
-    print('Que nome maneiro!')
-else:
-    print('Seu nome é normalzinho, nojento!')
-print('Bom dia {}!'.format(nome))
